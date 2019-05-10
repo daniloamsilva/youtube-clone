a1f76f0805039bd78b68b937c404c36fa94c5b35
Danilo Augusto <danilo_dams@yahoo.com.br>
2019-05-10 15:13:17 -0300

    Adicionando eventos de controle do vídeo

--- app/src/main/java/com/app/daniloaugustoyoutube/MainActivity.java
+++ app/src/main/java/com/app/daniloaugustoyoutube/MainActivity.java
@@ -14,10 +14,39 @@ public class MainActivity extends YouTubeBaseActivity
     private YouTubePlayerView youTubePlayerView;
     private static final String GOOGLE_API_KEY = "AIzaSyBtSmK-ixd68-6ud46pG2dtLY1BedE3H0w";
 
+    private YouTubePlayer.PlaybackEventListener playbackEventListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
+            @Override
+            public void onPlaying() {
+                Toast.makeText(MainActivity.this, "Vídeo executado", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPaused() {
+                Toast.makeText(MainActivity.this, "Vídeo pausado", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopped() {
+                Toast.makeText(MainActivity.this, "Vídeo parado", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onBuffering(boolean b) {
+                Toast.makeText(MainActivity.this, "Vídeo carregando", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSeekTo(int i) {
+                Toast.makeText(MainActivity.this, "SeekBar movimentado", Toast.LENGTH_SHORT).show();
+            }
+        };
 
         youTubePlayerView = findViewById(R.id.viewYoutubePlayer);
         youTubePlayerView.initialize(GOOGLE_API_KEY, this);
@@ -27,13 +56,15 @@ public class MainActivity extends YouTubeBaseActivity
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean foiRestaurado) {
         //youTubePlayer.loadVideo("Hy7hMIMb7rI");
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
         if (!foiRestaurado) {
-            youTubePlayer.cueVideo("Hy7hMIMb7rI");
+            //youTubePlayer.cueVideo("Hy7hMIMb7rI");
+            youTubePlayer.cuePlaylist("PLnRuoKy8h_Hvo7ILNrIsNVftjgu010Qc4");
         }
     }
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-        Toast.makeText(this, "Erro ao iniciar o Player", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Erro ao iniciar o Player" + youTubeInitializationResult.toString(), Toast.LENGTH_SHORT).show();
     }
 }
