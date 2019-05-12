c04acd0914d1fb22fd92c88a1481930a453f3e10
Danilo Augusto <danilo_dams@yahoo.com.br>
2019-05-12 00:30:45 -0300

    Criando interface do Youtube

--- app/src/main/java/com/app/daniloaugustoyoutube/MainActivity.java
+++ app/src/main/java/com/app/daniloaugustoyoutube/MainActivity.java
@@ -1,70 +1,23 @@
 package com.app.daniloaugustoyoutube;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerView;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends YouTubeBaseActivity
-                implements YouTubePlayer.OnInitializedListener {
-
-    private YouTubePlayerView youTubePlayerView;
     private static final String GOOGLE_API_KEY = "AIzaSyBtSmK-ixd68-6ud46pG2dtLY1BedE3H0w";
-
-    private YouTubePlayer.PlaybackEventListener playbackEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
-            @Override
-            public void onPlaying() {
-                Toast.makeText(MainActivity.this, "Vídeo executado", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onPaused() {
-                Toast.makeText(MainActivity.this, "Vídeo pausado", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onStopped() {
-                Toast.makeText(MainActivity.this, "Vídeo parado", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onBuffering(boolean b) {
-                Toast.makeText(MainActivity.this, "Vídeo carregando", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onSeekTo(int i) {
-                Toast.makeText(MainActivity.this, "SeekBar movimentado", Toast.LENGTH_SHORT).show();
-            }
-        };
-
-        youTubePlayerView = findViewById(R.id.viewYoutubePlayer);
-        youTubePlayerView.initialize(GOOGLE_API_KEY, this);
+        // Configuração da toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Youtube");
+        setSupportActionBar(toolbar);
 
     }
 
-    @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean foiRestaurado) {
-        //youTubePlayer.loadVideo("Hy7hMIMb7rI");
-        youTubePlayer.setPlaybackEventListener(playbackEventListener);
-        if (!foiRestaurado) {
-            //youTubePlayer.cueVideo("Hy7hMIMb7rI");
-            youTubePlayer.cuePlaylist("PLnRuoKy8h_Hvo7ILNrIsNVftjgu010Qc4");
-        }
-    }
-
-    @Override
-    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-        Toast.makeText(this, "Erro ao iniciar o Player" + youTubeInitializationResult.toString(), Toast.LENGTH_SHORT).show();
-    }
 }
