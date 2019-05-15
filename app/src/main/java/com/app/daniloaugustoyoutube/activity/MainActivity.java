ed77c39a5946bf393b5a3f9f530abb85fd2587ef
Danilo Augusto <danilo_dams@yahoo.com.br>
2019-05-15 20:31:29 -0300

    Configurando SearchView

--- app/src/main/java/com/app/daniloaugustoyoutube/activity/MainActivity.java
+++ app/src/main/java/com/app/daniloaugustoyoutube/activity/MainActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.app.daniloaugustoyoutube.R;
 import com.app.daniloaugustoyoutube.adapter.AdapterVideo;
 import com.app.daniloaugustoyoutube.model.Video;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Widgets
     private RecyclerView recyclerVideos;
+    private MaterialSearchView searchView;
 
     private List<Video> videos = new ArrayList<>();
     private AdapterVideo adapterVideo;
@@ -30,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Inicializar componentes
         recyclerVideos = findViewById(R.id.recyclerVideos);
+        searchView = findViewById(R.id.searchView);
 
         // Configuração da toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -43,6 +49,31 @@ public class MainActivity extends AppCompatActivity {
         recyclerVideos.setLayoutManager(new LinearLayoutManager(this));
         recyclerVideos.setAdapter(adapterVideo);
 
+        // Configuração dos métodos para SearchView
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
+            }
+        });
+
     }
 
     private void recuperarVideos(){
@@ -55,4 +86,14 @@ public class MainActivity extends AppCompatActivity {
         videos.add(video2);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_search);
+        searchView.setMenuItem(item);
+
+        return true;
+    }
 }
