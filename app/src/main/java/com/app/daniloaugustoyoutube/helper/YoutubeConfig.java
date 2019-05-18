427fe47bcb378a2793b41bc23b828fd4a10ef6c6
Danilo Augusto <danilo_dams@yahoo.com.br>
2019-05-18 20:34:39 -0300

    Introdução API Youtube

--- app/src/main/java/com/app/daniloaugustoyoutube/helper/YoutubeConfig.java
+++ app/src/main/java/com/app/daniloaugustoyoutube/helper/YoutubeConfig.java
@@ -2,8 +2,8 @@ package com.app.daniloaugustoyoutube.helper;
 
 public class YoutubeConfig {
 
-    public static String CANAL_ID = "";
+    public static String CANAL_ID = "UCVHFbqXqoYvEWM1Ddxl0QDg";
     public static String CHAVE_YOUTUBE_API = "AIzaSyBtSmK-ixd68-6ud46pG2dtLY1BedE3H0w";
-    public static String URL_BASE = "";
+    public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
 
 }
